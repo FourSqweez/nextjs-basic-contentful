d6a149eb7f8a8c85318107d474a9d1f3ce5d50cc
FourSqweez <four7518@gmail.com>
2021-05-07 22:53:48 +0700

    Added : react-masonry-css (grid component + responsive)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { createClient } from 'contentful'
 import RecipeCard from '../components/RecipeCard'
+import Masonry from 'react-masonry-css'
 
 export async function getStaticProps() {
   const client = createClient({
@@ -16,21 +17,23 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  return (
-    <div className="recipe-list">
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.sys.id} recipe={recipe} />
-      ))}
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  }
 
-      <style jsx>
-        {`
-          .recipe-list {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            grid-gap: 20px 60px;
-          }
-        `}
-      </style>
-    </div>
+  return (
+    <Masonry
+      breakpointCols={breakpoints}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {recipes.map((recipe) => (
+        <div key={recipe.sys.id}>
+          <RecipeCard recipe={recipe} />
+        </div>
+      ))}
+    </Masonry>
   )
 }
